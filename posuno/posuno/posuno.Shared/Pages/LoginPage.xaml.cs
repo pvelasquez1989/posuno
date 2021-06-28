1b5a7bda09ed3a814929e2f2ea1c3c7223bc5194
pvelasquez1989 <pccastano@gmail.com>
2021-06-27 21:43:03 -0500

    Basic login

---
+++ posuno/posuno/posuno.Shared/Pages/LoginPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using posuno.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace posuno.Pages
+{
+   
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValid = await ValidForm();
+            if(!isValid)
+            {
+                return;
+            }
+
+            MessageDialog messageDialog = new MessageDialog("Vamos bien!", "Ok");
+            await messageDialog.ShowAsync();
+
+
+        }
+        private async Task<bool> ValidForm()
+        {
+            MessageDialog messageDialog;
+
+            if(string.IsNullOrEmpty(EmailTextBox.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu email", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            if (!RegexUtilities.IsValidEmail(EmailTextBox.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar un email valido.", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(PasswordPasswordBox.Password))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu contraseña", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
